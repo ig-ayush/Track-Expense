6445acf3dd8f9f0a2c7d57e2500bb476cbcddc81
Ayush-alt-sys <igayushacharya@gmail.com>
2025-07-01 23:53:57 +0530

    style the balance block

--- src/Components/Tracker-component/Details.jsx
+++ src/Components/Tracker-component/Details.jsx
@@ -2,12 +2,21 @@ export function Details() {
     const userData = JSON.parse(localStorage.getItem("user-data"));
 
     return (
-        <main className="w-full h-fit p-10">
-            <div id="user-name" className="flex justify-center items-center absolute left-[15px] text-xl gap-5">
+        <main className="w-full h-fit flex flex-col gap-5">
+
+            <div id="user-name" className="flex  items-center text-xl gap-5 text-left">
                 <i className="fa-solid fa-user text-2xl"></i>
                 <h1 id="name" className="">{userData.user}</h1>
             </div>
-               <hr className="border-t border w-full my-4" />
+
+            <div id="balance-con" className="flex justify-center items-center border p-5">
+                <div id="balance">
+                    <div id="amount" className="flex flex-col items-center gap-3">
+                        <h1 className="text-6xl font-semibold">{userData.balance}</h1>
+                        <h2 className="text-sm text-green-500">Your Balance</h2>
+                    </div>
+                </div>
+            </div>
         </main>
     )
 }
--- src/Components/Tracker.jsx
+++ src/Components/Tracker.jsx
@@ -4,7 +4,7 @@ function Tracker({ setUserP }) {
 
     return(
         <main className=" w-screen h-screen flex justify-center items-center">
-            <div id="container" className="relative  w-[80ch] h-[90vh] border rounded-2xl flex flex-col items-center p-8">
+            <div id="container" className="relative  w-[60ch] h-[90vh] border rounded-2xl flex flex-col items-center p-8">
                 <Details />                
             </div>
         </main>
